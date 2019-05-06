09ff7f1af0e05dcb264695f8c10eae9ef027ef10
mayankgupta804 <mayank.gupta804@gmail.com>
2019-05-06 11:52:55 +0530

    Setup destination for uploading files

--- config.py
+++ config.py
@@ -5,3 +5,11 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'easy-peasy'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'product_viewer_app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TOP_LEVEL_DIR="/Users/mayank/Documents/Flask Projects/product_viewer"
+
+    # Uploads
+    UPLOADS_DEFAULT_DEST = TOP_LEVEL_DIR + '/project/static/csv/'
+    UPLOADS_DEFAULT_URL = 'http://127.0.0.1:5000/static/img/'
+    
+    UPLOADED_CSV_DEST = TOP_LEVEL_DIR + '/project/static/csv/'
+    UPLOADED_CSV_URL = 'http://127.0.0.1:5000/static/img/'
