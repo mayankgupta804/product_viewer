fb690d0fcb15a8924e0be687488bc51c446aa4ac
mayankgupta804 <mayank.gupta804@gmail.com>
2019-05-08 03:13:39 +0530

    Create models for Product and FileMetaData

--- product_viewer_app/models.py
+++ product_viewer_app/models.py
@@ -7,4 +7,14 @@ class Product(db.Model):
     description = db.Column(db.String(300))
 
     def __repr__(self):
-        return '<Product {}>'.format(self.sku)   
+        return '<Product {}>'.format(self.name)   
+
+class FileMetaData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(64))
+    name = db.Column(db.String(64), index=True, unique=True)
+    description = db.Column(db.String(300))
+    path = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Filename {}>'.format(self.name)  
